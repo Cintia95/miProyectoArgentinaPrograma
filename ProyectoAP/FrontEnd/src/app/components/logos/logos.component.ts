01cea96038eebaa08af02d36c5615d60e8426c27
Cintia95 <cintiabw1995@gmail.com>
2023-03-13 13:27:57 -0300

    base de datos conectada

--- ProyectoAP/FrontEnd/src/app/components/logos/logos.component.ts
+++ ProyectoAP/FrontEnd/src/app/components/logos/logos.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-logos',
   templateUrl: './logos.component.html',
   styleUrls: ['./logos.component.css']
 })
-export class LogosComponent {
+export class LogosComponent implements OnInit {
+  isLogged = false;
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+    // if(this.tokenService.getToken()){
+    //   this.isLogged = true;
+    // }else{
+    //   this.isLogged = false;
+    // }
+  }
+
+  onLogOut():void{
+    // this.tokenService.logOut();
+    window.location.reload();
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 
 }
